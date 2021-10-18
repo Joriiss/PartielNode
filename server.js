b80c52590e37d75f90e80be709fc588dfc877cf0
Joriiss <jorisvaux@gmail.com>
2021-10-18 22:47:24 +0200

    added database connection

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fs = require("fs");
+const { createConnection } = require("typeorm");
 const fileUpload = require("express-fileupload");
 
 const page = fs.readFileSync("index.html");
@@ -12,7 +13,28 @@ app.use(
       tempFileDir: "/tmp/",
     })
   );
-
+const connect = async () => {
+    try {
+      const connection = await createConnection({
+        name: "dbconnection1",
+        type: "mysql",
+        host: "localhost",
+        port: 3306,
+        username: "root",
+        password: "",
+        database: "nodepartiel",
+        entities: [__dirname + "/entity/*{.js,.ts}"],
+        synchronize: true
+      });
+      connection.query("SELECT * FROM souvenir", function (error, results, fields) {
+        if (error) throw error;
+        console.log(results);
+      });
+    } catch (e) {
+      console.log(e);
+    }
+};
+connect()
 
 
 app.get("/upload", (req, res) => {
@@ -28,7 +50,7 @@ app.post("/upload", async (req, res) => {
       res.send("File uploaded");
     });
   
-  });
+});
 
   
 
